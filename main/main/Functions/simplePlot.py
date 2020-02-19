543b865b0a1d4f6c9fddfbc6bc4770f614e6c857
Parsa <parsadaj@ut.ac.ir>
2020-02-19 15:27:10 +0330

    changed plot page

--- main/main/Functions/simplePlot.py
+++ main/main/Functions/simplePlot.py
@@ -1,36 +1,12 @@
 import numpy as np
 from bokeh.plotting import figure, output_file, save
 from bokeh.models import ColumnDataSource, CustomJS
-from bokeh.models.widgets import RangeSlider, TextInput
+from bokeh.models.widgets import RangeSlider, TextInput, CheckboxGroup
 from bokeh.layouts import row, column
 from bokeh.io import curdoc
 from bokeh.embed import components
 
 
-def create_callback(raw_source, source, range_slider, string):
-    callback = CustomJS(args=dict(
-                                raw_source=raw_source,
-                                source=source,
-                                range_slider=range_slider,
-                                ),
-                        code="""
-        const value = range_slider.value
-        const start = value[0]
-        const end = value[1]
-        const raw_data = raw_source.data;
-        const x = []
-        const y1 = []
-        const raw_x = raw_data['x']
-        const raw_{0} = raw_data['{0}']
-        for (var i = 0; i < end - start; i++) {
-            x[i] = raw_x[start + i]
-            {0}[i] = raw_{0}[start + i]
-        }
-        source.data['x'] = x
-        source.data['{0}'] = {0}
-        source.change.emit();
-    """)
-    return callback
 
 
 def plot_data(filename, cwd):
@@ -47,34 +23,67 @@ def plot_data(filename, cwd):
     width = 800
     height = 200
 
-    range_slider1 = RangeSlider(start=0, end=file.shape[0], value=(0, file.shape[0]), step=1, title="Range")
-    text_start1 = TextInput(value="", title="Start:", width=100)
-    text_end1 = TextInput(value="", title="End:", width=100)
-    p1 = figure(title="a_x", plot_width=width, plot_height=height)
-    p1.line(x='x', y='y1', source=source)
-    callback1 = create_callback(raw_source, source, range_slider1, 'y1')
-    range_slider1.js_on_change('value', callback1)
 
-    range_slider2 = RangeSlider(start=0, end=file.shape[0], value=(0, file.shape[0]), step=1, title="Range")
-    text_start2 = TextInput(value="", title="Start:", width=100)
-    text_end2 = TextInput(value="", title="End:", width=100)
-    p2 = figure(title="a_y", plot_width=width, plot_height=height)
-    p2.line(x='x', y='y2', source=source)
-    callback2 = create_callback(raw_source, source, range_slider2, 'y2')
-    range_slider2.js_on_change('value', callback2)
+    checkbox_group = CheckboxGroup(labels=["a_x", "a_y", "a_z"], active=[0, 1, 2])
+    range_slider = RangeSlider(start=0, end=file.shape[0], value=(0, file.shape[0]), step=1, title="Range1")
+    text_start = TextInput(value="", title="Start:", width=100)
+    text_end = TextInput(value="", title="End:", width=100)
 
-    range_slider3 = RangeSlider(start=0, end=file.shape[0], value=(0, file.shape[0]), step=1, title="Range")
-    text_start3 = TextInput(value="", title="Start:", width=100)
-    text_end3 = TextInput(value="", title="End:", width=100)
-    p3 = figure(title="a_z", plot_width=width, plot_height=height)
-    p3.line(x='x', y='y3', source=source)
-    callback3 = create_callback(raw_source, source, range_slider3, 'y3')
-    range_slider3.js_on_change('value', callback3)
+    p = figure(title="a_x", plot_width=width, plot_height=height)
+    l1 = p.line(x='x', y='y1', source=source, legend="a_x", line_color="red")
+    l2 = p.line(x='x', y='y2', source=source, legend="a_y", line_color="blue")
+    l3 = p.line(x='x', y='y3', source=source, legend="a_z", line_color="green")
 
-    layout1 = row(column(row(text_start1, text_end1), range_slider1), p1)
-    layout2 = row(column(row(text_start2, text_end2), range_slider2), p2)
-    layout3 = row(column(row(text_start3, text_end3), range_slider3), p3)
-    layout = column(layout1, layout2, layout3)
+    RangeSlider_callback = CustomJS(args=dict(
+                                raw_source=raw_source,
+                                source=source,
+                                range_slider=range_slider,
+                                ),
+                        code="""
+        const value = range_slider.value
+        const start = value[0]
+        const end = value[1]
+        const raw_data = raw_source.data;
+        const x = []
+        const y1 = []
+        const y2 = []
+        const y3 = []
+        const raw_x = raw_data['x']
+        const raw_y1 = raw_data['y1']
+        const raw_y2 = raw_data['y2']
+        const raw_y3 = raw_data['y3']
+        for (var i = 0; i < end - start; i++) {
+            x[i] = raw_x[start + i]
+            y1[i] = raw_y1[start + i]
+            y2[i] = raw_y2[start + i]
+            y3[i] = raw_y3[start + i]
+        }
+        source.data['x'] = x
+        source.data['y1'] = y1
+        source.data['y2'] = y2
+        source.data['y3'] = y3
+        source.change.emit();
+    """)
 
+    CheckboxGroup_callback = CustomJS(
+                                    args=dict(
+                                            checkbox_group=checkbox_group,
+                                            l1=l1,
+                                            l2=l2,
+                                            l3=l3
+                                            ),
+                                    code="""
+        active = checkbox_group.active
+
+        l1.visible = active.includes(0);
+        l2.visible = active.includes(1);
+        l3.visible = active.includes(2);
+    """)
+
+    range_slider.js_on_change('value', RangeSlider_callback)
+    checkbox_group.js_on_change('active', CheckboxGroup_callback)
+
+    my_widgets = row(checkbox_group, column(row(text_start, text_end), range_slider))
+    layout = column(my_widgets, p)
     script, div = components(layout)
     return script, div
