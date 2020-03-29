c69ab2108b44038cd77ab41a2907f9035785c2ca
mohammaddxgi@gmail.com <360dxgi.dll>
2020-03-29 10:00:54 +0430

    Add user Sign Up

--- main/users/urls.py
+++ main/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView, sucess_signup
+from .views import SignUpView, sucess_signup, SignUpView2
 from django.contrib.auth.decorators import login_required
 
 
@@ -7,4 +7,5 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('signup/', login_required(SignUpView.as_view()), name='signup'),
     path('signup/sucess',login_required(sucess_signup) ,name='success'),
+    path('signup2/', login_required(SignUpView2.as_view()), name='signup2'),
 ]
