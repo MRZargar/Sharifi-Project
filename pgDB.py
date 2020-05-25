fe2f72b58c30304e1661528ffea048b21492cc36
MRZargar <zargar.mr.k@gmail.com>
2020-05-25 21:23:31 +0430

    add dblink to pgDB module

--- pgDB.py
+++ pgDB.py
@@ -1,27 +1,20 @@
 import psycopg2
 
 class pgDB:
-    def __init__(self, host, database, user, password):
+    def __init__(self, host = "", database = "", user = "", password = ""):
+        self.remote_db = {}
         self.db = {}
         self.db["host"] = host
         self.db["database"] = database
         self.db["user"] = user
         self.db["password"] = password
 
-    # def __init__(self, initFile, section):
-    #     from configparser import ConfigParser
+    def dblink(self, host = "", database = "", user = "", password = ""):
+        self.remote_db["host"] = host
+        self.remote_db["database"] = database
+        self.remote_db["user"] = user
+        self.remote_db["password"] = password
 
-    #     parser = ConfigParser()
-    #     parser.read_file(initFile)
-
-    #     self.db = {}
-    #     if parser.has_section(section):
-    #         params = parser.items(section)
-    #         for param in params:
-    #             self.db[param[0]] = param[1]
-    #     else:
-    #         raise Exception('Section {0} not found in the {1} file'.format(section, initFile))
-    
     def __connect(self):
         self.conn = None
         self.conn = psycopg2.connect(**self.db)
@@ -29,6 +22,19 @@ class pgDB:
     def __disconnect(self):
         if self.conn is not None:
             self.conn.close()
+
+    def __dblink_get_connstr(self):
+        connstr = ""
+        if self.remote_db["host"] != "":
+            connstr += "host={}".format(self.remote_db["host"]) + " "
+        if self.remote_db["database"] != "":
+            connstr += "dbname={}".format(self.remote_db["database"]) + " "
+        if self.remote_db["user"] != "":
+            connstr += "user={}".format(self.remote_db["user"]) + " "
+        if self.remote_db["password"] != "":
+            connstr += "password={}".format(self.remote_db["password"])
+        
+        return connstr
 
     def __commit(self):
         self.conn.commit()
@@ -57,8 +63,24 @@ class pgDB:
             rows = cur.fetchall()
 
             cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
+
             self.__disconnect()
             return rows
+        except (Exception, psycopg2.DatabaseError) as error:
+            self.__disconnect()
+            print(error)
+    
+    def dblink_get(self, sql, AS):
+        connstr = self.__dblink_get_connstr()
+
+        sql = """select * from dblink('{}','{}') as {};""".format(connstr, sql, AS)
+        
+        return self.getQuery(sql)
+
+    def dblink_set(self, sql):
+        connstr = self.__dblink_get_connstr()
+
+        sql = """SELECT dblink_connect('{}');
+                select dblink('{}');""".format(connstr, sql)
+        
+        return self.getQuery(sql)
