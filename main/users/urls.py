96bd130461cd4f917e8cfdea5fbad33ef8fa2963
MohammadEbrahimy <mohammaddxgi@gmail.com>
2020-06-09 18:05:43 +0430

    edit urls and map and plot style

--- main/users/urls.py
+++ main/users/urls.py
@@ -18,10 +18,10 @@ urlpatterns = [
     path('signup/<int:pk>', SignUpView, name='signup'),
     path('active/users/<int:pk>', active_user_page, name="active_user_page"),
     path('signup/sucess',success_signup ,name='success'),
-    path('signup2/', SignUpView2, name='signup2'),
+    path('signup/user/', SignUpView2, name='signup2'),
     path('edit/<int:pk>', login_required(AuthorUpdate.as_view()), name="EditUserName"),
     path('changed', success_edit, name="success_edit"),
-    path('account_activation_sent/', account_activation_sent, name='account_activation_sent'),
+    path('account/activation/sent/', account_activation_sent, name='account_activation_sent'),
     path('activate/?P<uidb64>[0-9A-Za-z_\-]+/?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20}/', activate, name='activate'),
     path('profile/<int:pk>', profile_view, name='profile')
 
