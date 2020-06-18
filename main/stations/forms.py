2491585aa9ee26e37089676d2104da6ccea9b690
MohammadEbrahimy <mohammaddxgi@gmail.com>
2020-06-18 11:41:02 +0430

    <<<<<

--- main/stations/forms.py
+++ main/stations/forms.py
@@ -9,6 +9,21 @@ class StationSetup(forms.ModelForm):
 		fields = ['station_name', 'address', 'sensor_type', 'latitude', 'longitude', 'description']
 
 
+
+class StationSetup2(forms.ModelForm):
+
+	class Meta:
+		model =  Setup
+		fields = ['station_name', 'raspberryID', 'address', 'sensor_type', 'latitude', 'longitude', 'description',]
+		widgets = {'raspberryID': forms.Select()}
+
+	def __init__(self, *args, **kwargs):
+		new_choices = kwargs.pop('new_choices')
+		super().__init__(*args, **kwargs)
+		self.fields['raspberryID'] = forms.ChoiceField(choices=new_choices)
+
+
+
 class SetupFullForm(forms.ModelForm):
 	images = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
