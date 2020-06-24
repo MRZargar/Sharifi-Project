7471d8475185ac2f70eaff1b0d37001b0d7e6f9d
MohammadEbrahimy <mohammaddxgi@gmail.com>
2020-06-24 23:09:20 +0430

    edit raspberry functions

--- raspberry_functions/move_and_zip.py
+++ raspberry_functions/move_and_zip.py
@@ -2,24 +2,42 @@ import os
 import zipfile
 import shutil
 
-def zip_move(file_names, path):
-    path = path +"/"
+def zip_move(name):
+    path = "sent/"
     try:
         import zlib
         mode= zipfile.ZIP_DEFLATED
     except:
         mode= zipfile.ZIP_STORED
-    for name in file_names:
+    try:
         name = int(name[:-4])
-        try:
-            zipfile.ZipFile(str(name) + ".zip", 'w', mode).write(str(name) + ".txt")
-            shutil.move(str(name) + ".zip", path + str(name) + ".zip")
-            os.remove(str(name) + ".txt")
-        except OSError:
-            os.remove(str(name) + ".zip")
-            print(" Can not create zip file or move or remove %s file"% (str(name) +'.txt')) 
+    except Exception as ex:
+        print(ex)
+    try:
+        zipfile.ZipFile(str(name) + ".zip", 'w', mode).write(str(name) + ".txt")
+        shutil.move(str(name) + ".zip", path + str(name) + ".zip")
+        os.remove(str(name) + ".txt")
+    except OSError:
+        os.remove(str(name) + ".zip")
+        print(" Can not create zip file or move or remove %s file"% (str(name) +'.txt')) 
 
-path = 'sent'
-## create Zip file
-name = ['2110910800.txt', '2110907200.txt']
-zip_move(name, path)
+
+import os
+## make directory sented
+path = "sent"
+
+if os.path.isdir("sent"):
+    print("directory has exist")
+
+else:
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+
+## create Zip file and move in sent directory
+
+name = ''  # for example name = '2110910800.txt'
+zip_move(name)
