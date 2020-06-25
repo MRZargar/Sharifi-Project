19392eff8c306de6f55b2f4f840d8fef9c1ab796
MohammadEbrahimy <mohammaddxgi@gmail.com>
2020-06-25 13:51:46 +0430

    edit online function

--- raspberry_functions/online.py
+++ raspberry_functions/online.py
@@ -4,17 +4,23 @@ import time
 def name_to_number(name):
     return int(name[:-4])
 
-files = glob.glob("*.txt")
-print(files)
-name = max(list(map(name_to_number, files)))
-file_name = str(name) + ".txt"
-
+Loop = True
 temp_size = 0
-while True:
-    size = os.stat(file_name).st_size
-    if size > temp_size:
-        print("the file size has changed")
-    else:
-        print("the file size has not changed")
-    temp_size = size
-    time.sleep(5)
+while Loop:
+    try:
+        files = glob.glob("*.txt")
+        if len(files) != 0:
+            name = max(list(map(name_to_number, files)))
+            file_name = str(name) + ".txt"
+            size = os.stat(file_name).st_size
+            if size > temp_size:
+                print("the file size has changed")
+            else:
+                print("the file size has not changed")
+            temp_size = size
+            time.sleep(5)
+        else:
+            print("No file in this directory ")
+            time.sleep(1)
+    except Exception as ex:
+        print(ex)
