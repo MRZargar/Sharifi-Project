491f394ee50712f10f495ee2ed0d72365b874c17
MRZargar <zargar.mr.k@gmail.com>
2020-07-02 00:36:35 +0430

    get complete files and save on data base then zip_move them and finally send data to server

--- raspberry_functions/move_and_zip.py
+++ raspberry_functions/move_and_zip.py
@@ -1,8 +1,21 @@
 import os
 import zipfile
 import shutil
+import pandas as pd
+
+import glob
+import time
+from send_request.GeoLabAPI import GeoLabAPI
+from send_request.pgDB import pgDB
+
 
 path = "sent/"
+API = GeoLabAPI()
+DB = pgDB("localhost", "mydata", "postgres", "postgreut99")
+tableName = API.get_table_name(12345678)
+
+def name_to_number(name):
+    return int(name[:-4])
 
 def zip_move(name):
     try:
@@ -33,7 +46,47 @@ else:
     else:
         print (">> move_and_zip / info:  Successfully created the directory %s " % path)
 
-## create Zip file and move in sent directory
 
-name = ''  # for example name = '2110910800.txt'
-zip_move(name)
+
+Loop = True
+temp_size = 0
+while Loop:
+    try:
+        files = glob.glob("*.txt")
+        if len(files) != 0:
+            name = max(list(map(name_to_number, files)))
+            current_file = str(name) + ".txt"
+            for file in files:
+                if file == current_file: continue
+
+                f = pd.read_csv(file, sep=' ', header=["T", "AX", "AY", "AZ", "TEMP"])
+                for counter in range(3):
+                    try:
+                        query = ""
+                        for inx, data in f.iterrows():
+                            query += "insert into data(t, a_x, a_y, a_z, temp) values ({},{},{},{},{});\n".format(data["T"], data["AX"], data["AY"], data["AZ"], data["TEMP"])
+
+                        DB.setQuery(query)
+                    except Exception as ex:
+                        print(">> move_and_zip / error:  %d. The %s don't saved on local database.\n" % (counter + 1, file) ,ex)
+                        continue
+                    else:
+                        print(">> move_and_zip / info:  The %s saved on local database." % file)
+                        zip_move(file)
+                        break
+            
+            data_count = 0
+            try:
+                dontSentData = DB.getQuery("select * from data where not is_sent")
+                API.send_data(tableName, dontSentData)
+            except Exception as ex:
+                print(">> move_and_zip / error:  send data faild.\n" ,ex)
+            else:
+                print(">> move_and_zip / info:  %d from datas sent." % data_count)
+            
+            time.sleep(5)
+        else:
+            print(">> move_and_zip / info:  No file in this directory ")
+            time.sleep(1)
+    except Exception as ex:
+        print(">> move_and_zip / error:  read files faild\n" ,ex)
