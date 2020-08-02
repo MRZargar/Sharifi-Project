246c6586c07449331d7569229042d82ee696781d
MohammadEbrahimy <mohammaddxgi@gmail.com>
2020-08-02 11:03:37 +0430

    download app ready change

--- main/Tasks/updater.py
+++ main/Tasks/updater.py
@@ -2,10 +2,9 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from .download_link import create_download_link, delete_download_link
 from .send_active_email import send_email
-
 def start():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(deamon=False)
+    scheduler.start()
     scheduler.add_job(create_download_link, 'interval', minutes=2)
     scheduler.add_job(delete_download_link, 'interval', days=1)
     scheduler.add_job(send_email, 'interval', minutes=5)
-    scheduler.start()
