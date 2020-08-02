b2fc16d0c32e977f820a0b07325059919abe561c
MohammadEbrahimy <mohammaddxgi@gmail.com>
2020-08-02 17:33:41 +0430

    users app edit

--- main/users/admin.py
+++ main/users/admin.py
@@ -7,17 +7,17 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'phone_number', 'userType', 'is_staff','email_confirmed', 'admin_confirmed']
+    list_display = ['email', 'username', 'phone_number', 'userType', 'is_staff','email_confirmed', 'admin_confirmed', 'active_email_send']
     fieldsets = (
     	(None, {'fields': ('username', 'email', 'phone_number', 'userType', 'password')}),
-    	('permissions', {'fields':('is_staff', 'is_active', 'email_confirmed', 'admin_confirmed')}),
+    	('permissions', {'fields':('is_staff', 'is_active', 'email_confirmed', 'admin_confirmed', 'active_email_send')}),
     	)
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('username', 'email', 'phone_number', 'userType','password1', 'password2',
-                       'is_staff', 'is_active', 'email_confirmed', 'admin_confirmed'
+                       'is_staff', 'is_active', 'email_confirmed', 'admin_confirmed', 'active_email_send',
                        )
             }
         ),
